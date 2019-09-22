f232054545c6d1a0ff53eb6daa118b61dca3c61c
James Carlson <jxxcarlson@mac.com>
2019-09-23 01:30:47 +0200

    Working on Request.elm

--- src/Request.elm
+++ src/Request.elm
@@ -1,4 +1,4 @@
-module Request exposing (RequestMsg(..), createDocument, documentsByAuthor, update_document, delete_document)
+module Request exposing (RequestMsg(..), createDocument)
 
 import Graphql.Http
 import Graphql.SelectionSet as SelectionSet exposing (SelectionSet, with)
@@ -15,9 +15,9 @@ import Api.Scalar exposing(Id(..))
 
 type RequestMsg =
        GotNewDocument (RemoteData (Graphql.Http.Error Document) Document)
-     | GotUserDocuments (RemoteData (Graphql.Http.Error (Maybe (List Document))) (Maybe (List Document)))
-     | ConfirmUpdatedDocument (RemoteData (Graphql.Http.Error (Maybe Document)) (Maybe Document))
-     | ConfirmUDeleteDocument (RemoteData (Graphql.Http.Error (Maybe Document)) (Maybe Document))
+--     | GotUserDocuments (RemoteData (Graphql.Http.Error (Maybe (List Document))) (Maybe (List Document)))
+--     | ConfirmUpdatedDocument (RemoteData (Graphql.Http.Error (Maybe Document)) (Maybe Document))
+--     | ConfirmUDeleteDocument (RemoteData (Graphql.Http.Error (Maybe Document)) (Maybe Document))
 
 
 endpoint = "https://math-markdown-heroku.herokuapp.com/v1/graphql"
@@ -27,34 +27,34 @@ authorizationToken =  "x-hasura-admin-secret: abc"
 
 createDocument : Document -> Cmd RequestMsg
 createDocument document =
-    Mutation.insert_document (createDocumentRequiredArguments document) documentSelectionSet
+    Mutation.insert_document identity documentSelectionSet
          |> Graphql.Http.mutationRequest endpoint
          |> Graphql.Http.withHeader "authorization" authorizationToken         -- |> Graphql.Http.withHeader "Access-Control-Allow-Origin:" "*"
          |> Graphql.Http.send (RemoteData.fromResult >> GotNewDocument)
 
 
-documentsByAuthor : String -> Cmd RequestMsg
-documentsByAuthor authorId =
-    Query.documentsByAuthor  { author = authorId } (SelectionSet.list [documentSelectionSet])
-      |> Graphql.Http.queryRequest endpoint
-      |> Graphql.Http.withHeader "authorization" authorizationToken
-      |> Graphql.Http.send (RemoteData.fromResult >> GotUserDocuments)
-
-
-update_document : Document -> Cmd RequestMsg
-update_document document =
-    Mutation.update_document (updatedDocumentRequiredArguments document) documentSelectionSet
-         |> Graphql.Http.mutationRequest endpoint
-         |> Graphql.Http.withHeader "authorization" authorizationToken
-         |> Graphql.Http.send (RemoteData.fromResult >> ConfirmUpdatedDocument)
-
-
-delete_document : Document -> Cmd RequestMsg
-delete_document document =
-    Mutation.delete_document { id = document.id } documentSelectionSet
-         |> Graphql.Http.mutationRequest endpoint
-         |> Graphql.Http.withHeader "authorization" authorizationToken
-         |> Graphql.Http.send (RemoteData.fromResult >> ConfirmUDeleteDocument)
+--documentsByAuthor : String -> Cmd RequestMsg
+--documentsByAuthor authorId =
+--    Query.documentsByAuthor  { author = authorId } (SelectionSet.list [documentSelectionSet])
+--      |> Graphql.Http.queryRequest endpoint
+--      |> Graphql.Http.withHeader "authorization" authorizationToken
+--      |> Graphql.Http.send (RemoteData.fromResult >> GotUserDocuments)
+--
+--
+--update_document : Document -> Cmd RequestMsg
+--update_document document =
+--    Mutation.update_document (updatedDocumentRequiredArguments document) documentSelectionSet
+--         |> Graphql.Http.mutationRequest endpoint
+--         |> Graphql.Http.withHeader "authorization" authorizationToken
+--         |> Graphql.Http.send (RemoteData.fromResult >> ConfirmUpdatedDocument)
+--
+--
+--delete_document : Document -> Cmd RequestMsg
+--delete_document document =
+--    Mutation.delete_document { id = document.id } documentSelectionSet
+--         |> Graphql.Http.mutationRequest endpoint
+--         |> Graphql.Http.withHeader "authorization" authorizationToken
+--         |> Graphql.Http.send (RemoteData.fromResult >> ConfirmUDeleteDocument)
 
 -- IMPLEMENTATION
 
@@ -64,15 +64,16 @@ updatedDocumentRequiredArguments document =
     { id = document.id
     , data = documentInput document }
 
-createDocumentRequiredArguments : Document -> Mutation.CreateDocumentRequiredArguments
+createDocumentRequiredArguments : Document -> Mutation.InsertDocumentRequiredArguments
 createDocumentRequiredArguments document =
     { data = documentInput document }
 
-documentInput : Document -> InputObject.DocumentInput
+documentInput : Document -> InputObject.Document_insert_input
 documentInput document =
-        {   identifier = document.identifier
+        {   id = document.id
+          , identifier = document.identifier
           , title = document.title
-          , author = document.authorIdentifier
+          , authorIdentifier = document.authorIdentifier
           , content = document.content
           , tags = document.tags
           , timeCreated = Time.posixToMillis document.timeCreated // 1000
