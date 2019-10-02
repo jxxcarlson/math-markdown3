f99c0b6cfc788491d85a2dd4337fb62b88b312f2
James Carlson <jxxcarlson@mac.com>
2019-10-02 12:30:44 +0200

    Update welcome document

--- src/Data.elm
+++ src/Data.elm
@@ -37,7 +37,6 @@ image =
 
 rhsUserText =
     """
-
 # Welcome!
 
 ## About this app
@@ -53,15 +52,40 @@ $$
 Formulas written in TeX/LaTeX are rendered using [MathJax](https://mathjax.org)
 
 We are growing the app into a content management system which
-can be useful both for personal notes and classroom materials. Lecture note,
-problem sets, and the like.  We also plan to add live editing and rendering
-of LaTeX documents (MiniLaTeX flavor).
+can be useful both for personal notes and classroom materials: problem sets, handouts, lecture notes,
+and the like.  We also plan to add  live editing and rendering
+of LaTeX documents (MiniLaTeX flavor), as well as the ability to convert Markdown documents to MiniLaTeX.
 
-For more information, click the **Read** tab and take a look at the article *A Pure Elm Markdown Parser*.
+For more information, click the **Read** tab (or do ctrl-R).  Some public articles to look at:
 
-## Hints
+-  *A Pure Elm Markdown Parser*
+- *Keyboard commands*
+- *Search and Sort*
+- *News and Plans*
 
-blah blah
+## Tips
 
-##  News
+### Getting info/help
+
+To find the documents for this app, put `docs/k` or `help/k` in the search box.  The `k` is for *keyword search*.
+
+### Reading documents
+
+ To read documents, press the **Read** button or do `ctrl R` .  More about keyboard shortcuts in the docs.  See previous tip.
+
+### Saving documents
+
+ When you are editing a document, it is saved every four seconds if it has changes.  Look at the red/green indicator in the footer. To force a save, press **Save** or do `ctrl-S` .
+
+## News
+
+### Images
+
+ Use the model `![TITLE::left](IMAGE LOCATION)` to float an image on the left.
+
+To float on the right use `![TITLE::right](IMAGE LOCATION)`
+
+The plain vanilla version, `![TITLE](IMAGE LOCATION)` scales the image to 100%.
+
+For a smaller centered image, use `![TITLE::center](IMAGE LOCATION)`
 """
