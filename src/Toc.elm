dcd3c0960636aec57072ed431a356153d249140a
James Carlson <jxxcarlson@mac.com>
2019-10-09 19:15:21 +0200

    Comments

--- src/Toc.elm
+++ src/Toc.elm
@@ -89,6 +89,36 @@ hasChildren_ tree =
     Just (Tree.children tree /= [])
 
 
+{-|
+
+     traverse f tree maps one tree to another
+     using a function f : Tree a -> Tree a
+
+    ff tree =
+        List.length (Tree.children tree)
+
+
+    updateff : Tree ( Int, Int ) -> Tree ( Int, Int )
+    updateff tree =
+        let
+            ( a, _ ) =
+                Tree.label tree
+
+            newLabel =
+                ( a, ff tree )
+        in
+        tree |> Tree.replaceLabel newLabel
+
+
+    tt = tree (0,0) [tree (1,0) [tree (2,0) [], tree (3,0) [] ], tree (4,0) [tree (5,0) []]]
+         Tree (0,0) [Tree (1,0) [Tree (2,0) [],Tree (3,0) []],Tree (4,0) [Tree (5,0) []]]
+         : Tree ( number, number1 )
+
+    traverse Toc.updateff tt
+      Just (Tree (0,2) [Tree (1,2) [Tree (2,0) [],Tree (3,0) []],Tree (4,1) [Tree (5,0) []]])
+         : Maybe (Tree ( Int, Int ))
+
+-}
 traverse : (Tree a -> Tree a) -> Tree a -> Maybe (Tree a)
 traverse f tree =
     let
@@ -97,10 +127,6 @@ traverse f tree =
             Maybe.map (Zipper.mapTree f) (Just (Zipper.fromTree tree))
     in
     Maybe.map Zipper.toTree (traverse_ f maybeZipper)
-
-
-
--- Maybe.map Zipper.toTree maybeZipper
 
 
 traverse_ : (Tree a -> Tree a) -> Maybe (Zipper a) -> Maybe (Zipper a)
@@ -140,10 +166,10 @@ Note that while this function modifies the label, it depends on the tree
 updateHasChildren : Tree TocItem -> Tree TocItem
 updateHasChildren tree =
     let
-        la =
+        label_ =
             Tree.label tree
     in
-    tree |> Tree.replaceLabel { la | hasChildren = hasChildren_ tree }
+    tree |> Tree.replaceLabel { label_ | hasChildren = hasChildren_ tree }
 
 
 {-| Toggle the visibility of the children of the
