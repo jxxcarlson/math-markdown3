7bc93f6172c6349a1988499c7489da61f870924b
James Carlson <jxxcarlson@mac.com>
2019-10-30 11:29:07 -0400

    Change make : only --optimize

--- make.sh
+++ make.sh
@@ -1,9 +1,9 @@
-if [ $1 = "-d" ]
-then
-  elm make  --debug src/Main.elm --output=public/main.js
-else
-  elm make  --optimize src/Main.elm --output=public/main.js
-fi
+#if [ $1 = "-d" ]
+#then
+#  elm make  --debug src/Main.elm --output=public/main.js
+#else
+#  elm make  --optimize src/Main.elm --output=public/main.js
+#fi
 
 
-# elm make  --optimize src/Main.elm --output=public/main.js
+elm make  --optimize src/Main.elm --output=public/main.js
