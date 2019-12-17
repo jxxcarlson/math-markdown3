0c46ed8ef57d98a54f1c8d326c781892c6690632
James Carlson <jxxcarlson@mac.com>
2019-12-17 11:38:13 +0100

    Still working on the parcel config

--- public/index.js
+++ public/index.js
@@ -2,6 +2,14 @@
 
 import { Elm } from '../src/Main.elm'
 
+import * as MarkdownEditor from './assets/MarkdownEditor.js'
+import '/assets/custom-element-config.js'
+import '/assets/math-text.js'
+import '/assets/navigation.js'
+import '/assets/outside.js'
+
+MarkdownEditor.defineWebComponent()
+
 // Generate random numbers to see UUID generator
 const crypto = window.crypto || window.msCrypto;
 const getRandomInts = (n) => {
@@ -26,4 +34,3 @@ var app = Elm.Main.init({node: root, flags:  {
  })
 
 
-MarkdownEditor.defineWebComponent()
